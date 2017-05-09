24d5a252b8e0ef5eb9601cfbc9fee0a762968d2d
Alejandro <alejandro.valencia@e3learning.com.au>
2017-05-10 09:16:08 +0930

    Project created

---
+++ e3test/src/main/java/com/e3/E3testApplication.java
@@ -0,0 +1,12 @@
+package com.e3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E3testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E3testApplication.class, args);
+	}
+}
