f641d056c747f5c63dbec3f2efb4c5c5a145e1ce
Pathum Athukorala <pathum@Pathums-MacBook-Pro-M1.local>
2023-06-24 18:07:59 +0930

    implement delete company functionality

--- src/main/java/com/e3/test/contoller/CompanyController.java
+++ src/main/java/com/e3/test/contoller/CompanyController.java
@@ -1,6 +1,8 @@
 package com.e3.test.contoller;
 
 import com.e3.test.repository.CompanyRepository;
+import com.e3.test.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.e3.test.entity.Company;
@@ -9,15 +11,19 @@ import com.e3.test.entity.Company;
 @RequestMapping("/company")
 public class CompanyController {
 
+	private CompanyService companyService;
 
-	private final CompanyRepository companyRepository;
-
-	public CompanyController(CompanyRepository companyRepository) {
-		this.companyRepository = companyRepository;
+	public CompanyController(CompanyService companyService) {
+		this.companyService = companyService;
 	}
 
 	@RequestMapping(value = "/{companyId}", method = RequestMethod.GET)
 	public @ResponseBody Company getCompany(@PathVariable Long companyId) {
-		return companyRepository.findOne(companyId);
+		return companyService.findCompanyById(companyId);
+	}
+
+	@RequestMapping(value = {"/{companyId}"}, method = RequestMethod.DELETE)
+	public void deleteCompany(@PathVariable Long companyId) {
+		companyService.deleteCompanyAfterVerification(companyId);
 	}
 }
